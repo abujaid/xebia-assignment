99585957be2f25ea364dc587b9a71a7e828f070d
Abu Zaid <zaid.abuzaid707@gmail.com>
2019-11-18 12:58:45 +0530

    assignment

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer from './reducers'
+
+const store = createStore(
+    rootReducer,
+    compose(
+        (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+        compose
+    )
+)
+
+export default store;
